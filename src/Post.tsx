d3f37f81919134922eeefbf46abcb89bf763428a
Ben-Or Habari <benorhabari97@gmail.com>
2022-09-13 17:50:23 +0300

    inital commit

---
+++ src/Post.tsx
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from "react";
+
+async function getJson(url: string) {
+    const res = await fetch(url);
+    return res.json();
+};
+
+export default function Post({post}: {post?: IPost}) {
+    const [numLikes, addLike] = useState(0);
+    const [dynamicPost, setDynamicPost] = useState({title:'', id:''});
+    useEffect(() => {
+        async function setDynamicData() {
+            setDynamicPost(await getJson(`./data/posts.json`))            
+        }
+
+        if(!post){
+            setDynamicData();
+        };
+    }, [post])
+    return <div>
+      <h2>{post?.title || dynamicPost?.title}</h2>
+      <p>Num likes {numLikes}</p>
+      <Like onLike={ () => addLike(numLikes + 1) }>
+        <span>Like</span>
+      </Like>
+    </div>
+};
+
+interface IPost {
+    id: string;
+    title: string;
+}
+  
+
+interface IPropsLike{
+    onLike: () => void;
+    children: JSX.Element;
+}
+
+function Like({onLike, children}: IPropsLike){
+    return <button onClick={onLike}>Like</button>
+}
